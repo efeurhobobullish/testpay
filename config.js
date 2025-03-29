c6fae429c429ed835d92f4f53afe8abbfa3c9eb1
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-29 01:58:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: 3000, // Change if needed
-  MONGO_URI: "your_mongodb_connection_string",
-  JWT_SECRET: "your_jwt_secret",
+  MONGO_URI: "mongodb+srv://empiretech:kkVDB0ooTlgIsH11@cluster0.js7ra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  JWT_SECRET: "-@Pqnap+@(/1jAPPnew/@10",
   PAYSTACK_SECRET_KEY: "your_paystack_secret_key",
   PAYSTACK_PUBLIC_KEY: "your_paystack_public_key",
   BASE_URL: "https://empiretech.biz.id"
