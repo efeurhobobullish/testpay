5ce0c837c8edc073e75d293564090b4d78e19eed
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-29 01:39:00 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: 3000, // Change if needed
+  MONGO_URI: "your_mongodb_connection_string",
+  JWT_SECRET: "your_jwt_secret",
+  PAYSTACK_SECRET_KEY: "your_paystack_secret_key",
+  PAYSTACK_PUBLIC_KEY: "your_paystack_public_key",
+  BASE_URL: "https://empiretech.biz.id"
+};
